00acee7528bf6c276a715620d9366a35543f3b9c
Eduardo Hamilko <eduardohamilks@gmail.com>
2021-12-06 22:53:46 -0300

    chore: improve swagger documentation

--- src/modules/reminders/models/reminder.model.ts
+++ src/modules/reminders/models/reminder.model.ts
@@ -2,15 +2,15 @@ import { ApiProperty } from '@nestjs/swagger'
 import { Entity } from '@common/classes'
 
 export class Reminder extends Entity {
-  @ApiProperty()
+  @ApiProperty({ example: 'Reunião de Negócios' })
   name: string
 
-  @ApiProperty()
+  @ApiProperty({ example: '2021-12-18T00:00:00.000Z' })
   date: Date
 
-  @ApiProperty()
+  @ApiProperty({ example: 'Curitiba, Paraná, Brasil' })
   location: string
 
-  @ApiProperty()
+  @ApiProperty({ example: '5e8f8f8f-8f8f-8f8f-8f8f-8f8f8f8f8f8f' })
   userId: string
 }
